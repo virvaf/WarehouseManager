3978e5f70f8bcbf22a9ef9644f0800dda83be425
V-Software <v-software@seznam.cz>
2026-09-05 18:58:55 +0200

    Product class setup

---
+++ WarehouseManager/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseManager
+{
+    internal class Product
+    {
+        public string Name { get; private set; }
+        public string Location { get; private set; }
+
+
+
+    }
+}
