6ad7a75309085e53cf5a2523192930501f6d66b8
V-Software <v-software@seznam.cz>
2026-09-06 19:01:00 +0200

    update

--- WarehouseManager/Product.cs
+++ WarehouseManager/Product.cs
@@ -6,10 +6,37 @@ namespace WarehouseManager
 {
     internal class Product
     {
+        private int stock;
+        private decimal price;
+
         public string Name { get; private set; }
         public string Location { get; private set; }
+        public int ProductId { get; private set; }
 
+        public int Stock 
+        {
+            get { return stock; } 
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Stock count cannot be negative");
+                }
+                stock = value;
+            }
+        }
 
-
+        public decimal Price
+        {
+            get { return price; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Price cannot be negative.");
+                }
+                price = value;
+            }
+        }
     }
 }
